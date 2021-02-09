3b47e6c7e23ef6e67e8071700495206d5c732129
Clément Robert <cr52@protonmail.com>
2021-02-09 23:02:48 +0100

    test: test for errors

--- tests/test_main.py
+++ tests/test_main.py
@@ -53,3 +53,38 @@ def test_inplace_substitution(tmp_path, capsys, flag: str):
 
     actual = target.read_text()
     assert actual == expected
+
+
+def test_missing_target(tmp_path, capsys):
+    target = tmp_path / "hello.txt"
+    target.write_text(content)
+    subs_file = tmp_path / "subs.json"
+    with open(subs_file, "w") as fh:
+        json.dump(subs, fh)
+
+    typo_target = target.with_suffix(".this_file_doesnt_exists")
+    assert not typo_target.exists()
+
+    retval = main([str(typo_target), str(subs_file)])
+    assert retval != 0
+
+    out, err = capsys.readouterr()
+    assert out == ""
+    assert err == f"Error: {typo_target} not found.\n"
+
+
+def test_broken_call(tmp_path, capsys, monkeypatch):
+
+    monkeypatch.setattr("apply_subs.main.BASE_COMMAND", ["not_a_real_command $HOME"])
+    target = tmp_path / "hello.txt"
+    target.write_text(content)
+    subs_file = tmp_path / "subs.json"
+    with open(subs_file, "w") as fh:
+        json.dump(subs, fh)
+
+    retval = main([str(target), str(subs_file)])
+    assert retval != 0
+
+    out, err = capsys.readouterr()
+    assert out == ""
+    assert err == f"Error: failed to apply subsitutions to {target}\n"
