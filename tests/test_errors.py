f80e917b48d0f339039b78f3e2d9d9cf99fc2308
Clément Robert <cr52@protonmail.com>
2021-02-12 23:38:32 +0100

    relocate tests

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -3,6 +3,33 @@ import json
 from apply_subs.main import main
 
 
+def test_missing_target(simple_setup, capsys):
+    target, subs_file, expected = simple_setup
+
+    typo_target = target.with_suffix(".this_file_doesnt_exists")
+    assert not typo_target.exists()
+
+    retval = main([str(typo_target), str(subs_file)])
+    assert retval != 0
+
+    out, err = capsys.readouterr()
+    assert out == ""
+    assert err == f"Error: {typo_target} not found.\n"
+
+
+def test_broken_call(simple_setup, capsys, monkeypatch):
+    target, subs_file, expected = simple_setup
+
+    monkeypatch.setattr("apply_subs.main.BASE_COMMAND", ["not_a_real_command $HOME"])
+
+    retval = main([str(target), str(subs_file)])
+    assert retval != 0
+
+    out, err = capsys.readouterr()
+    assert out == ""
+    assert err == f"Error: failed to apply subsitutions to {target}\n"
+
+
 def test_invalid_schema(tmp_path, capsys):
     target = tmp_path / "hello.txt"
     target.write_text("nothing")
--- tests/test_main.py
+++ tests/test_main.py
@@ -27,30 +27,3 @@ def test_inplace_substitution(simple_setup, capsys, flag: str):
 
     actual = target.read_text()
     assert actual == expected
-
-
-def test_missing_target(simple_setup, capsys):
-    target, subs_file, expected = simple_setup
-
-    typo_target = target.with_suffix(".this_file_doesnt_exists")
-    assert not typo_target.exists()
-
-    retval = main([str(typo_target), str(subs_file)])
-    assert retval != 0
-
-    out, err = capsys.readouterr()
-    assert out == ""
-    assert err == f"Error: {typo_target} not found.\n"
-
-
-def test_broken_call(simple_setup, capsys, monkeypatch):
-    target, subs_file, expected = simple_setup
-
-    monkeypatch.setattr("apply_subs.main.BASE_COMMAND", ["not_a_real_command $HOME"])
-
-    retval = main([str(target), str(subs_file)])
-    assert retval != 0
-
-    out, err = capsys.readouterr()
-    assert out == ""
-    assert err == f"Error: failed to apply subsitutions to {target}\n"
